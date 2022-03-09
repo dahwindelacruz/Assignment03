9ad180cdeba73f6d4af532ed8b23104f0fceb064
Dahwin DelaCruz <dahwin.delacruz@gmail.com>
2022-03-09 13:36:59 -0800

    updated

---
+++ js/02-coinflip-streak.js
@@ -0,0 +1,9 @@
+let coinFlip;
+do {
+    coinFlip = Math.round(Math.random() * 1);
+    if (coinFlip == 0) {
+        console.log(`Heads`);
+    } else {
+        console.log(`Tails`);
+    }
+} while (coinFlip === 0);
